c57963935e6fd49587d4a7885806bf21d94719e2
Justin Rich <jrich523@gmail.com>
2018-05-02 10:33:42 -0700

    more complex user script for testing

--- usrScript.js
+++ usrScript.js
@@ -1,4 +1,15 @@
 module.exports = function(devices){
     console.log(devices['usr3'].state)
     devices['usr3'].toggle()
+    if(devices['white'].state)
+    {
+        console.log('switch active, light on')
+        devices['usr1'].on()
+    }
+    else
+    {
+        console.log('switch inactive, light off')
+        devices['usr1'].off()
+    }
+    
 }
