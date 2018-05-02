518338a2cd492d7d76b495b217e0fdaed63b73f6
Justin Rich <jrich523@gmail.com>
2018-05-02 10:42:18 -0700

    added logging

--- devicemanager/devices/input.js
+++ devicemanager/devices/input.js
@@ -10,6 +10,7 @@ module.exports =  class Input {
         b.pinMode(this.pin,b.INPUT)
         b.attachInterrupt(this.pin, true, b.CHANGE, (x) =>{
             console.log(this.name + ': switch state updated to ' + this._value)
+            console.log(x)
             this._value = x
         })
         /*
