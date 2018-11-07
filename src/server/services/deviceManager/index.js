8dd35a778e14d253f80b030bc00ba50d09c676ca
Justin Rich <jrich523@gmail.com>
2018-11-07 11:10:27 -0800

    light refactor

--- src/server/services/deviceManager/index.js
+++ src/server/services/deviceManager/index.js
@@ -7,21 +7,24 @@ module.exports = (function() {
             this.Devices = {};
             this.Configuration = require('../../config/deviceconfig.json')
             this.DeviceLibrary = require('./devices')
+
             this.LoadDevices()
     
         }
         
         LoadDevices(){
             //todo: add a default state? or figure out how to tell the current state?
-            var d = this.Configuration.devices
-            d.forEach((i) => {
+            this.Configuration.devices.forEach((i) => {
                 this._NewDevice(i)
             })
             this.isLoaded = true;
         }
         
+        //todo: will likely need some sort of reload to rebuild if the config has changed without starting?
+
+
         _NewDevice(data){
-                this.Devices[data.name] = this.DeviceLibrary[data.type].Create(data.name,data.cfg)
+            this.Devices[data.name] = this.DeviceLibrary[data.type].Create(data.name,data.cfg)
         }
     
     }
